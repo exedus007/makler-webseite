95be6a164322e26aa64ddffff11e3469d31c01f5
exedus007 <mehmetaskin@web.de>
2026-04-09 10:49:31 +0200

    Update mailer-common.php

--- mailer-common.php
+++ mailer-common.php
@@ -6,7 +6,6 @@ use PHPMailer\PHPMailer\PHPMailer;
 function respond(int $statusCode, array $data): void
 {
     http_response_code($statusCode);
-    header('Content-Type: application/json; charset=utf-8');
     echo json_encode($data, JSON_UNESCAPED_UNICODE);
     exit;
 }
